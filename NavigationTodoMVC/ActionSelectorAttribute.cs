8fbe1abc7bec0bcecacfd0b68f1531f7ea62206a
Graham Mendick <graham.mendick@gmail.com>
2015-01-03 19:18:42 +0000

    Added comments

--- NavigationTodoMVC/ActionSelectorAttribute.cs
+++ NavigationTodoMVC/ActionSelectorAttribute.cs
@@ -4,8 +4,19 @@ using System.Web.Mvc;
 
 namespace NavigationTodoMVC
 {
+	/// <summary>
+	/// Allows the same route to be served by different Actions.
+	/// </summary>
 	public class ActionSelectorAttribute : ActionNameSelectorAttribute
 	{
+		/// <summary>
+		/// Determines which Action processes the request by matching the value
+		/// of an incoming action parameter against the Action name.
+		/// </summary>
+		/// <param name="controllerContext">Controller context.</param>
+		/// <param name="actionName">Action Name.</param>
+		/// <param name="methodInfo">Action method information.</param>
+		/// <returns>True if the Action matches, false otherwise.</returns>
 		public override bool IsValidName(ControllerContext controllerContext, string actionName, MethodInfo methodInfo)
 		{
 			if (!controllerContext.IsChildAction)
