23033820477b614a7aaa411a9c92dad7ff2aaf65
Graham Mendick <graham.mendick@gmail.com>
2015-03-29 17:01:03 +0100

    Javascript tweak

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,8 @@
+var newDiv = document.createElement("div"); 
+var newContent = document.createTextNode("test"); 
+newDiv.appendChild(newContent);
+var currentDiv = document.getElementById("test"); 
+document.body.insertBefore(newDiv, currentDiv); 
 document.getElementById('test').addEventListener('click', function(){
 	console.log('test');
 });
